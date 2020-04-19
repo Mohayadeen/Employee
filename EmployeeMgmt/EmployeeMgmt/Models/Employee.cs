63d9fc4403011f95afa0d00243dc4cdd0950fb37
MABDULK8 <mabdulk8@ford.com>
2020-04-19 18:28:14 +0530

    Initial Version

---
+++ EmployeeMgmt/EmployeeMgmt/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeMgmt.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public double Salary { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ EmployeeMgmt/EmployeeMgmt/Models/EmployeeDataContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeMgmt.Models
+{
+    public partial class EmployeeDataContext : DbContext
+    {
+        public EmployeeDataContext()
+        {
+        }
+
+        public EmployeeDataContext(DbContextOptions<EmployeeDataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.Designation)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
